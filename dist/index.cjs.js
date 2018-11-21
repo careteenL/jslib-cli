184ee429fad19fed1099323e885c3a2df48d0dbb
careteenL <15074806497@163.com>
2018-11-21 10:21:08 +0800

    fix: remove ignore dist folder

---
+++ dist/index.cjs.js
@@ -0,0 +1,14 @@
+/*!
+ * jslib-cli v0.1.0
+ * (c) 2018-2018 careteenL <15074806497@163.com>
+ * Released under the MIT License.
+ */
+'use strict';
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+function add(a, b) {
+  return a + b;
+}
+
+exports.add = add;
---
+++ dist/index.esm.js
@@ -0,0 +1,10 @@
+/*!
+ * jslib-cli v0.1.0
+ * (c) 2018-2018 careteenL <15074806497@163.com>
+ * Released under the MIT License.
+ */
+function add(a, b) {
+  return a + b;
+}
+
+export { add };
